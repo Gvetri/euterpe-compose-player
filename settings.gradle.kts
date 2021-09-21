0dc1bd37857c7a79bc229a17af97c1086f94a1f2
Giuseppe Vetri <giuseppe@codingpizza.com>
2021-09-21 09:05:57 +0200

    Update dependency versions.properties

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,9 @@ pluginManagement {
 
 plugins {
     id("de.fayard.refreshVersions") version "0.10.1"
+////                            # available:"0.11.0"
+////                            # available:"0.20.0"
+////                            # available:"0.21.0"
 }
 rootProject.name = "Euterpe"
 
